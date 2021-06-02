eb45d3dc5eb75c10eb31603ea0aba262f1ee29f7
Anirban Das <allmail.anirban@gmail.com>
2021-06-02 10:29:25 +0530

    initial commit

--- src/app/weatherDetails/weatherDetails.component.ts
+++ src/app/weatherDetails/weatherDetails.component.ts
@@ -1,16 +1,32 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'weather-details',
-  templateUrl: './weatherDetails.component.html',
-  styleUrls: ['./weatherDetails.component.scss']
+  selector: "weather-details",
+  templateUrl: "./weatherDetails.component.html",
+  styleUrls: ["./weatherDetails.component.scss"],
 })
-
 export class WeatherDetails implements OnInit {
   @Input() weatherData: data[];
+  data: data = null;
 
   ngOnInit() {
+    // [{
+    //     "name": "San Jose",
+    //     "temperature": "36º F",
+    //     "wind": "31Kmph",
+    //     "humidity": "66%"
+    //   }]
+  }
 
+  fetchWeather(event): void {
+    // console.log(event.target.value);
+    this.data = null;
+    this.weatherData.forEach((element: data) => {
+      if (element.name.toLowerCase() === event.target.value.toLowerCase()) {
+        this.data = element;
+        return;
+      }
+    });
   }
 }
 
